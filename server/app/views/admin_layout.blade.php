227b2f99d489c5870e62378b0809b3cc9a087bd5
fmcgrath <fmcgrath@98d3ac28-6462-4f9b-a335-089c6823c7e4>
2014-11-17 16:07:29 +0000

    admin menu, select current page with js
    git-svn-id: svn+ssh://source.la.utexas.edu/repos/lrc-site/trunk@16 98d3ac28-6462-4f9b-a335-089c6823c7e4

--- server/app/views/admin_layout.blade.php
+++ server/app/views/admin_layout.blade.php
@@ -4,23 +4,55 @@
         <meta name='viewport' content='width=device-width, initial-scale=1'>
         <title>LRC Admin - @yield('title')</title>
  
-        {{ HTML::style('//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css') }}
-        {{ HTML::style('//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css') }}
+        {{ HTML::style('//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css') }}
+        {{ HTML::style('//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css') }}
         
+        {{ HTML::script('https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js') }}
+        {{ HTML::script('http://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js') }}
+  
         <style>
             body {
-                margin-top: 5%;
+                margin-top: 70px;
             }
         </style>
+
     </head>
     <body>
-        {{ HTML::link('logout', 'Logout', array('title' => 'Logout' )) }}
-        <div class='container-fluid'>
-            <div class='row'>
-                @yield('content')
-            </div>
-        </div>
+    
+    <script type="text/javascript">
+    $(document).ready(function(){
+		//highlight the menu for whichever page we are on
+		$('a[href$="' + this.location.pathname + '"]').parent().addClass('active');
+    });
+    </script>
+    
+    	@if(Request::path() != 'login')
+    
+	        <!-- navbar -->
+		    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+		      <div class="container">
+		        <div class="navbar-header">
+		          <div class="navbar-brand">LRC Admin</div>
+		        </div>
+		        
+		        <ul class="nav navbar-nav">
+		          <li>{{ HTML::link('/admin/', 'Menu', array('title' => 'Admin Menu')) }}</li>
+		          <li>{{ HTML::link('/admin/user', 'Users', array('title' => 'User Maintenance' )) }}</li>
+		        </ul>
+		        
+		        <ul class="nav navbar-nav navbar-right">
+		          <li>{{ HTML::link('logout', 'Logout', array('title' => 'Logout' )) }}</li>
+		        </ul>
+		      </div>
+		    </nav>
+	    
+	    @endif
+	
+	    <div class="container-fluid">		
+	        @yield('content')
+	    </div>
+ 
     </body>
-</html>
+</html>	
 
 
