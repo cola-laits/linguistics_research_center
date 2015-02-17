948867842b82abdfc040798258929eb433467b84
fmcgrath <fmcgrath@98d3ac28-6462-4f9b-a335-089c6823c7e4>
2015-02-17 15:54:08 +0000

    changed start.sh to see if it helps get docker working on server
    git-svn-id: svn+ssh://source.la.utexas.edu/repos/lrc-site/trunk@108 98d3ac28-6462-4f9b-a335-089c6823c7e4

--- start.sh
+++ start.sh
@@ -4,6 +4,9 @@
 MIGRATE_ENV='local'
 [ -n "${APP_ENV}" ] && MIGRATE_ENV=${APP_ENV}-migrate
 php artisan migrate --env=${MIGRATE_ENV}
+
+#rerun chmod to open up permissions on any files that got written as part of the migration.
+chmod -R 777 /laravel_storage
  
 # Now start Apache.
 apache2-foreground
