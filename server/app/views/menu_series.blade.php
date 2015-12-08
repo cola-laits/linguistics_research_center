654d475b2857545cbca1430ea02c92a541568908
fmcgrath <fmcgrath@98d3ac28-6462-4f9b-a335-089c6823c7e4>
2015-12-08 23:31:07 +0000

    made series menu accordian
    git-svn-id: svn+ssh://source.la.utexas.edu/repos/lrc-site/trunk@232 98d3ac28-6462-4f9b-a335-089c6823c7e4

--- server/app/views/menu_series.blade.php
+++ server/app/views/menu_series.blade.php
@@ -1,6 +1,10 @@
 <ul class="side-nav">
-	<li class="office"><label>Lessons</label></li>
-	@foreach($lesson_menu as $lesson_menu_item)
-        <li>{{ HTML::link('eieol_lesson/' . $lesson_menu_item->id, str_replace(' Online', '', $lesson_menu_item->menu_name), array('title' => $lesson_menu_item->expanded_title )) }} </li>
-    @endforeach
-</ul>
+	<li>
+		<a class="off-canvas-submenu-call" href="#">Other Language Lessons</a>
+		<ul class="side-nav">
+			@foreach($lesson_menu as $lesson_menu_item)
+		        <li>{{ HTML::link('eieol_lesson/' . $lesson_menu_item->id, str_replace(' Online', '', $lesson_menu_item->menu_name), array('title' => $lesson_menu_item->expanded_title )) }} </li>
+		    @endforeach
+	  	</ul>
+	</li>
+</ul> 
