05f8afdfbc0e310dc3f7399259076e51e3ebf0dc
fmcgrath <fmcgrath@98d3ac28-6462-4f9b-a335-089c6823c7e4>
2016-01-26 16:51:42 +0000

    put lesson menu back to non-accordian.  It stopped working.  And I didn't really like it anyway.  So there.
    git-svn-id: svn+ssh://source.la.utexas.edu/repos/lrc-site/trunk@255 98d3ac28-6462-4f9b-a335-089c6823c7e4

--- server/app/views/menu_series.blade.php
+++ server/app/views/menu_series.blade.php
@@ -1,10 +1,6 @@
 <ul class="side-nav">
-	<li>
-		<a class="off-canvas-submenu-call" href="#">Other Language Lessons</a>
-		<ul class="side-nav">
-			@foreach($lesson_menu as $lesson_menu_item)
-		        <li>{{ HTML::link('eieol_lesson/' . $lesson_menu_item->id, str_replace(' Online', '', $lesson_menu_item->menu_name), array('title' => $lesson_menu_item->expanded_title )) }} </li>
-		    @endforeach
-	  	</ul>
-	</li>
-</ul> 
+	<li class="office"><label>Lessons</label></li>
+	@foreach($lesson_menu as $lesson_menu_item)
+        <li>{{ HTML::link('eieol_lesson/' . $lesson_menu_item->id, str_replace(' Online', '', $lesson_menu_item->menu_name), array('title' => $lesson_menu_item->expanded_title )) }} </li>
+    @endforeach
+</ul>
