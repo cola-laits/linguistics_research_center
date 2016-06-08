91dd92e0c231ea40b5d9ded2dd083d9084606ea1
fmcgrath <fmcgrath@98d3ac28-6462-4f9b-a335-089c6823c7e4>
2016-06-08 19:28:07 +0000

    Fixed links to css to use new server
    git-svn-id: svn+ssh://source.la.utexas.edu/repos/lrc-site/trunk@274 98d3ac28-6462-4f9b-a335-089c6823c7e4

--- server/app/views/layout_header.blade.php
+++ server/app/views/layout_header.blade.php
@@ -9,21 +9,21 @@ but also used to make printable lessons in the public controller -->
 <meta content="width=device-width, initial-scale=1.0 maximum-scale=1" name="viewport"/>
 
 <!-- FAVICONS -->
-    <link href="https://www.utexas.edu/cola/_internal/images/favicons/favicon.ico" rel="icon"/>
-    <link href="https://www.utexas.edu/cola/_internal/images/favicons/apple-touch-icon-180x180.png" rel="apple-touch-icon" sizes="180x180"/>
-    <link href="https://www.utexas.edu/cola/_internal/images/favicons/apple-touch-icon-152x152.png" rel="apple-touch-icon" sizes="152x152"/>
-    <!--[if IE]><link rel="shortcut icon" href="http://www.utexas.edu/cola/_internal/images/favicons/favicon.ico" /><![endif]-->
-    <meta content="https://www.utexas.edu/cola/_internal/images/faviacons/apple-touch-icon-144x144.png" name="msapplication-TileImage"/>
+    <link href="https://liberalarts.utexas.edu/_internal/images/favicons/favicon.ico" rel="icon"/>
+    <link href="https://liberalarts.utexas.edu/_internal/images/favicons/apple-touch-icon-180x180.png" rel="apple-touch-icon" sizes="180x180"/>
+    <link href="https://liberalarts.utexas.edu/_internal/images/favicons/apple-touch-icon-152x152.png" rel="apple-touch-icon" sizes="152x152"/>
+    <!--[if IE]><link rel="shortcut icon" href="https://liberalarts.utexas.edu/_internal/images/favicons/favicon.ico" /><![endif]-->
+    <meta content="https://liberalarts.utexas.edu/_internal/images/faviacons/apple-touch-icon-144x144.png" name="msapplication-TileImage"/>
     <meta content="#ffffff" name="msapplication-TileColor"/>
     <meta content="UT Austin" name="apple-mobile-web-app-title"/>
-    <link href="https://www.utexas.edu/cola/_internal/images/favicons/apple-touch-icon-144x144.png" rel="apple-touch-icon" sizes="144x144"/>
-    <link href="https://www.utexas.edu/cola/_internal/images/favicons/apple-touch-icon-120x120.png" rel="apple-touch-icon" sizes="120x120"/>
-    <link href="https://www.utexas.edu/cola/_internal/images/favicons/apple-touch-icon.png" rel="apple-touch-icon"/>
-    <link href="https://www.utexas.edu/cola/_internal/images/favicons/apple-touch-icon-57x57.png" rel="apple-touch-icon" sizes="57x57"/>
-    <link href="https://www.utexas.edu/cola/_internal/images/favicons/apple-touch-icon-60x60.png" rel="apple-touch-icon" sizes="60x60"/>
-    <link href="https://www.utexas.edu/cola/_internal/images/favicons/apple-touch-icon-76x76.png" rel="apple-touch-icon" sizes="76x76"/>
-    <link href="https://www.utexas.edu/cola/_internal/images/favicons/apple-touch-icon-72x72.png" rel="apple-touch-icon" sizes="72x72"/>
-    <link href="https://www.utexas.edu/cola/_internal/images/favicons/apple-touch-icon-114x114.png" rel="apple-touch-icon" sizes="114x114"/>
+    <link href="https://liberalarts.utexas.edu/_internal/images/favicons/apple-touch-icon-144x144.png" rel="apple-touch-icon" sizes="144x144"/>
+    <link href="https://liberalarts.utexas.edu/_internal/images/favicons/apple-touch-icon-120x120.png" rel="apple-touch-icon" sizes="120x120"/>
+    <link href="https://liberalarts.utexas.edu/_internal/images/favicons/apple-touch-icon.png" rel="apple-touch-icon"/>
+    <link href="https://liberalarts.utexas.edu/_internal/images/favicons/apple-touch-icon-57x57.png" rel="apple-touch-icon" sizes="57x57"/>
+    <link href="https://liberalarts.utexas.edu/_internal/images/favicons/apple-touch-icon-60x60.png" rel="apple-touch-icon" sizes="60x60"/>
+    <link href="https://liberalarts.utexas.edu/_internal/images/favicons/apple-touch-icon-76x76.png" rel="apple-touch-icon" sizes="76x76"/>
+    <link href="https://liberalarts.utexas.edu/_internal/images/favicons/apple-touch-icon-72x72.png" rel="apple-touch-icon" sizes="72x72"/>
+    <link href="https://liberalarts.utexas.edu/_internal/images/favicons/apple-touch-icon-114x114.png" rel="apple-touch-icon" sizes="114x114"/>
 <!-- END FAVICONS -->
 
 <!-- SYSTEM-PAGE-TITLE and META-DATA-->
@@ -33,7 +33,7 @@ but also used to make printable lessons in the public controller -->
 <!-- CSS LOADING -->
     <link href="https://fonts.googleapis.com/css?family=Roboto:400,300,700,700italic,400italic|Roboto+Condensed:400,300" rel="stylesheet" type="text/css"/>
     <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet" type="text/css"/>
-    <link href="//www.utexas.edu/cola/_internal/css/app_units.css" rel="stylesheet" type="text/css"/>
+    <link href="https://liberalarts.utexas.edu/_internal/css/app_units.css" rel="stylesheet" type="text/css"/>
     
 	{{ HTML::style('css/lrcstyle.css') }}
 	{{ HTML::style('//code.jquery.com/ui/1.11.2/themes/smoothness/jquery-ui.css') }}
@@ -43,7 +43,7 @@ but also used to make printable lessons in the public controller -->
 
 <!--  "Must Have in the Header" Javascript -->
 	<script src="//jwpsrv.com/library/1O4izvC8EeKppRIxOQulpA.js" type="text/javascript"></script> <!-- JW PLAYER -->
-	<script src="//www.utexas.edu/cola/_internal/js/modernizr.js" type="text/javascript"></script>
+	<script src="https://liberalarts.utexas.edu/_internal/js/modernizr.js" type="text/javascript"></script>
 	{{ HTML::script('//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js') }}
 	{{ HTML::script('//cdn.datatables.net/1.10.6/js/jquery.dataTables.min.js') }}
 <!-- END "Must Have in the Header" Javascript -->
