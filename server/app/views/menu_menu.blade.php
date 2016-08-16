5eface1eee72bd5ee2b8896e515649f2461f1739
Chris Pittman <chrispittman@austin.utexas.edu>
2016-08-16 17:49:49 -0500

    Change link wording as per Todd Krause email

--- server/app/views/menu_menu.blade.php
+++ server/app/views/menu_menu.blade.php
@@ -1,5 +1,5 @@
 <ul class="side-nav">
-	<li><a href="{{$static_site}}">Return to the LRC Site</a></li>
+	<li><a href="{{$static_site}}">LRC Home</a></li>
 	<li><a href="/eieol">EIEOL Lessons</a></li>
 	<li><a href="/lex">Lexicon</a></li>
 </ul>
