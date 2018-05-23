2fe64c1253ad498f7d0e70b2274d94426dd99110
Geoff Boyd <geoff.boyd@austin.utexas.edu>
2018-05-23 16:13:30 +0200

    rewrite glossed text parser for clarity and order-agnosticism

--- server/app/models/EieolGlossedText.php
+++ server/app/models/EieolGlossedText.php
@@ -18,92 +18,104 @@ class EieolGlossedText extends Eloquent {
 	{
 		//this makes a new version of the glossed text with span tags for each gloss.  
 		//Then you can make them clickable so they toggle the gloss.
+		
 		$read_str = $this->glossed_text;
-		$read_str = str_replace("<br/>", "<br/> ", $read_str);
-		$read_str = str_replace("<br />", "<br /> ", $read_str);
-		$read_str = str_replace("<br>", "<br> ", $read_str);
-		$read_str = str_replace("</p>", "</p> ", $read_str);
-		
-		$punctuation = array(",",".","!","?",":","(",")");		
-		$new_str = '';
-		
-		foreach($this->glosses as $gloss){
+    
+    $glosses = [];
+    foreach($this->glosses as $g) {
+      $gloss['surface_form'] = $g->surface_form;
+      $gloss['id'] = $g->pivot->id;
+      $glosses[] = $gloss;
+    }
+    
+    //order glosses from longest to short for greedy matching!!
+    usort( $glosses, 
+    function ($a, $b) {
+    return strlen($a['surface_form']) < strlen($b['surface_form']);
+    } );
 
-			$start = mb_stripos($read_str, $gloss->surface_form, 0, 'UTF-8');
-			$len = mb_strlen($gloss->surface_form, 'UTF-8');
-			//print $gloss->surface_form . ' ' . $start . ' ' . $len . '<br/>';
-			
-			//if you get here, the gloss didn't exactly match the glossed text.  Let's try to guess.
-			if ($start === false) {
-				$in_tag = false;
-				
-				//print '____alert<br/>';
-				
-				//the gloss might contain space.  If so we'll skip that many spaces.
-				$num_spaces = substr_count(strip_tags(trim($gloss->surface_form)), ' ');
-				
-				for ($i=0; $i < mb_strlen($read_str,'UTF-8'); $i++) {
-					
-					$char = mb_substr($read_str, $i, 1, 'UTF-8');
-					//if we are in an html tag, skip until we get out
-					if ($in_tag) {
-						if ($char == '>') {
-							$in_tag = false;
-						}
-						continue;
-					}
-					if ($char == '<') {
-						$in_tag = true;
-						continue;
-					}
-					
-					//skip punctuation
-					if (in_array($char, $punctuation)){
-						continue;
-					}
-					
-					//set the start as the first non blank character
-					if ($start === false && $char != ' ') {
-						$start = $i;
-					}
-					//the end is the first space after the start, unless we have some to skip
-					if ($start !== false && $char == ' ') {
-						if ($num_spaces > 0) {
-							$num_spaces -= 1;
-							continue;
-						}
-						$len = $i-$start;
-						break;
-					}
-				}
-				
-			}
-			
-// 			print $read_str . '<br/>' .
-// 				  mb_substr($read_str, 0, $start, 'UTF-8') . ' ' . 
-// 				  $gloss->surface_form . 
-// 				  ' start=' . $start . 
-// 				  ' length=' . $len . ' ' . 
-// 				  mb_substr($read_str, $start, $len, 'UTF-8') . '<br/>';
+		foreach($glosses as $gloss) {
+      $read_str = $this->attachClicks($read_str, $gloss['surface_form'], $gloss['id']);
+      $read_str = $this->attachClicks($read_str, ucfirst($gloss['surface_form']), $gloss['id']);
+    }
 		
-			//add everything in front of the gloss to the new string
-			$new_str .= mb_substr($read_str, 0, $start, 'UTF-8');
+		return $read_str;
 		
-			//add the gloss with a tag to the new str
-			$new_str .= '<a href="#" onclick="return false;" class="click_gloss" id="pivot_' . $gloss->pivot->id . '">' .
-					mb_substr($read_str, $start, $len, 'UTF-8') .
-					'</a>';
-		
-			//trim of the front of the old string
-			$read_str = mb_substr($read_str, $start + $len, null, 'UTF-8');
-			
-//    			print $new_str . '<br/>' . $read_str . '<hr/>';
-		}
-			
-		//add rest of old string to end of new string
-		$new_str .=  $read_str;
-		
-		return $new_str;
 	} 
+	
+	private function attachClicks($str,$g,$id) 
+	{
+	    $punctuation = array(",",".","!","?",":","(",")","։","՝","յ");	  
+	    
+	    $a = '<a href="#" onclick="return false;" class="click_gloss" id="pivot_' . $id . '">';
+	    
+	    foreach ($punctuation as $p) {
+        $str = $this->mbReplace(' '.$g.$p, ' '.$a.$g.$p.'</a> ', $str);
+        $str = $this->mbReplace($p.$g.' ', $p.$a.$g.'</a> ', $str);
+      }
+      
+      $str = $this->mbReplace(' '.$g.' ', ' '.$a.$g.'</a> ', $str);
+      
+      if ($this->startsWith($str, $g)) {
+        $str = $a.mb_substr($str, 0, mb_strlen($g)).'</a>'.mb_substr($str, mb_strlen($g));
+      }
+      
+      if ($this->endsWith($str, $g)) {
+        $str = mb_substr($str, 0, mb_strlen($str) - mb_strlen($g)).$a.$g.'</a>';
+      }
+      
+      return $str;
+	}
+	
+  private function startsWith($haystack, $needle)
+  {
+       preg_match('/'.preg_quote($needle).'/ui', $haystack, $matches, PREG_OFFSET_CAPTURE);
+       
+       foreach ($matches as $m) {
+          if ($m[1] == 0) return true;
+       }
+  
+       return false;
+  }
+
+  private function endsWith($haystack, $needle)
+  {
+      $length = mb_strlen($needle);
+
+      return $length === 0 || 
+      (mb_substr($haystack, - $length) === $needle);
+  }
+  
+  private function mbReplace($search, $replace, $subject, $encoding = 'UTF-8', &$count=0) 
+  {
+  
+  //  $retval = preg_replace('/'.preg_quote($search).'/ui', $replace, $subject);
+  //  return $retval;
+
+    if(!is_array($subject)) {
+        $searches = is_array($search) ? array_values($search) : [$search];
+        $replacements = is_array($replace) ? array_values($replace) : [$replace];
+        $replacements = array_pad($replacements, count($searches), '');
+        foreach($searches as $key => $search) {
+            $replace = $replacements[$key];
+            $search_len = mb_strlen($search, $encoding);
+
+            $sb = [];
+            while(($offset = mb_strpos($subject, $search, 0, $encoding)) !== false) {
+                $sb[] = mb_substr($subject, 0, $offset, $encoding);
+                $subject = mb_substr($subject, $offset + $search_len, null, $encoding);
+                ++$count;
+            }
+            $sb[] = $subject;
+            $subject = implode($replace, $sb);
+        }
+    } else {
+        foreach($subject as $key => $value) {
+            $subject[$key] = self::mbReplace($search, $replace, $value, $encoding, $count);
+        }
+    }
+    return $subject;
+    
+  }
 
 }
