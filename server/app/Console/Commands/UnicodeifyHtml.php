44a92474d76bb06e2e46802aeafbae34ab1f5f24
Chris Pittman <chrispittman@austin.utexas.edu>
2019-04-11 20:45:23 -0500

    Add Laravel cmd for manually-run html-formatting cleanup
    Possibly fixes #48

---
+++ server/app/Console/Commands/UnicodeifyHtml.php
@@ -0,0 +1,121 @@
+<?php
+
+namespace App\Console\Commands;
+
+use DB;
+use Illuminate\Console\Command;
+use Normalizer;
+
+class UnicodeifyHtml extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'lrc:unicodify_html';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Convert HTML markup to its corresponding Unicode markup.';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        \Log::info('updating lex_etyma');
+        $rows = DB::select('SELECT * FROM lex_etyma');
+        foreach ($rows as $row) {
+            DB::update('update lex_etyma'.
+                ' SET entry=?'.
+                ' WHERE id=?',[
+                Normalizer::normalize($this->clean_markup($row->entry)),
+                $row->id
+            ]);
+        }
+
+        \Log::info('updating lesson');
+        $rows = DB::select('SELECT * FROM eieol_lesson');
+        foreach ($rows as $row) {
+            DB::update('update eieol_lesson'.
+                ' SET intro_text=?'.
+                ' WHERE id=?',[
+                Normalizer::normalize($this->clean_markup($row->intro_text)),
+                $row->id
+            ]);
+        }
+
+        \Log::info('updating head_word');
+        $rows = DB::select('SELECT * FROM eieol_head_word');
+        foreach ($rows as $row) {
+            DB::update('update eieol_head_word'.
+                ' SET word=?'.
+                ' WHERE id=?',[
+                Normalizer::normalize($this->clean_markup($row->word)),
+                $row->id
+            ]);
+        }
+
+        \Log::info('updating glossed_text');
+        $rows = DB::select('SELECT * FROM eieol_glossed_text');
+        foreach ($rows as $row) {
+            DB::update('update eieol_glossed_text'.
+                ' SET glossed_text=?'.
+                ' WHERE id=?',[
+                Normalizer::normalize($this->clean_markup($row->glossed_text)),
+                $row->id
+            ]);
+        }
+
+        \Log::info('updating gloss');
+        $rows = DB::select('SELECT * FROM eieol_gloss');
+        foreach ($rows as $row) {
+            DB::update('update eieol_gloss'.
+                ' SET surface_form=?'.
+                ' WHERE id=?',[
+                Normalizer::normalize($this->clean_markup($row->surface_form)),
+                $row->id
+            ]);
+        }
+
+        \Log::info('updating grammar');
+        $rows = DB::select('SELECT * FROM eieol_grammar');
+        foreach ($rows as $row) {
+            DB::update('update eieol_grammar'.
+                ' SET title=?'.
+                ' ,grammar_text=?'.
+                ' WHERE id=?',[
+                Normalizer::normalize($this->clean_markup($row->title)),
+                Normalizer::normalize($this->clean_markup($row->grammar_text)),
+                $row->id
+            ]);
+        }
+    }
+
+    protected function clean_markup($str) {
+        $str = str_replace("<sup><font size=\"+1\">́</font></sup>","́", $str);
+        $str = str_replace("<sup><font size=\"+1\">̀</font></sup>", "̀", $str);
+        $str = str_replace("<sup><font size='+1'>̀</font></sup>", "̀", $str);
+        $str = str_replace("<sup><font size='+1'>́</font></sup>","́", $str);
+        $str = str_replace("<sup>́</sup>","́", $str);
+        $str = str_replace("<sup>̀</sup>","̀", $str);
+        $str = str_replace("<sup>̃</sup>","̃", $str);
+        return $str;
+    }
+}
