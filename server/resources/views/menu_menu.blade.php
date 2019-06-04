2c02c33585dc89b3ee10f1bc0653de8aeb6616f7
Chris Pittman <chrispittman@austin.utexas.edu>
2019-06-04 15:01:14 -0500

    UI mockup

--- server/resources/views/menu_menu.blade.php
+++ server/resources/views/menu_menu.blade.php
@@ -1,6 +1,27 @@
 <ul class="side-nav">
     <li><a href="http://www.utexas.edu/cola/centers/lrc/">LRC Home</a></li>
-    <li><a href="/eieol">EIEOL Lessons</a></li>
-    <li><a href="/lex">Lexicon</a></li>
+    <li class="open"><a href="/eieol">EIEOL Lessons</a>
+        <ul class="no-bullet">
+            <li><a href="#">Overview</a></li>
+            <li><a href="#">How-To</a></li>
+            <li><a href="#">Lessons</a></li>
+            <li><ul class="no-bullet">
+                    <li><a href="#">Albanian</a></li>
+                    <li><a href="#">Armenian</a></li>
+                    <li><a href="#">Armenian - Romanized</a></li>
+                    <li><a href="#">Baltic</a></li>
+                    <li><a href="#">etc....</a></li>
+                </ul></li>
+        </ul>
+    </li>
+    <li><a href="/lex">IE Lexicon</a>
+        <ul class="no-bullet">
+            <li><a href="#">Overview</a></li>
+            <li><a href="#">How-To</a></li>
+            <li><a href="#">Pokorny</a></li>
+            <li><a href="#">Language Index</a></li>
+            <li><a href="#">Semantic Index</a></li>
+        </ul>
+    </li>
 </ul>
 
