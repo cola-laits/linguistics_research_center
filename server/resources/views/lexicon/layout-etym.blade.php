99cedd3d3a657e7370a262214b3364dbfd133db7
Chris Pittman <chrispittman73@gmail.com>
2022-12-16 12:26:51 -0600

    lexicon word-search impl

--- server/resources/views/lexicon/layout-etym.blade.php
+++ server/resources/views/lexicon/layout-etym.blade.php
@@ -1,45 +1,7 @@
 @extends('lexicon.layout')
 
-@section('search-sidebar')
-    <div id="sidebar-header" class="sidebar-header">
-        <p>[FIXME search bar here]</p>
-        <p>
-            <a href="#" class="selector_type_link{{$selected_sidebar=='headword' ? ' selected' : ''}}"
-               onclick="choose_selector_type('headword');return false;" id="selector_type_link_headword">Headwords</a>
-            |
-            <a href="#" class="selector_type_link{{$selected_sidebar=='category' ? ' selected' : ''}}"
-               onclick="choose_selector_type('category');return false;" id="selector_type_link_category">Categories</a>
-        </p>
-    </div>
-    <div id="sidebar-content" class="sidebar-content">
-        <div class="selector_type" id="selector_type_headword"@if ($selected_sidebar!=='headword') style="display:none;"@endif>
-            <ul>
-                @foreach ($lexicon->etyma as $etymon)
-                    <li data-sidebar-id="{{$etymon->id}}"><sup>*</sup><a href="/lexicon/{{$lexicon->slug}}/etymon/{{$etymon->id}}">{{$etymon->entry}}</a></li>
-                @endforeach
-            </ul>
-        </div>
-        <div class="selector_type" id="selector_type_category"@if ($selected_sidebar!=='category') style="display:none;"@endif>
-            <div class="accordion" id="accordionCategory">
-                @foreach ($lexicon->semantic_categories as $category)
-                <div class="accordion-item">
-                    <h2 class="accordion-header" id="accordion_category_heading_{{$category->id}}">
-                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#accordion_category_collapse_{{$category->id}}" aria-expanded="true" aria-controls="accordion_category_collapse_{{$category->id}}">
-                            {{$category->text}}
-                        </button>
-                    </h2>
-                    <div id="accordion_category_collapse_{{$category->id}}" class="accordion-collapse collapse" aria-labelledby="accordion_category_heading_{{$category->id}}" data-bs-parent="#accordionCategory">
-                        <div class="accordion-body">
-                            <ul>
-                                @foreach ($category->semantic_fields as $field)
-                                    <li data-sidebar-id="{{$field->id}}"><a href="/lexicon/{{$lexicon->slug}}/field/{{$field->id}}">{{$field->text}}</a></li>
-                                @endforeach
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-                @endforeach
-            </div>
-        </div>
-    </div>
+@section('search-item-list')
+    @foreach ($lexicon->etyma as $etymon)
+        <li data-sidebar-id="{{$etymon->id}}"><sup>*</sup><a href="/lexicon/{{$lexicon->slug}}/etymon/{{$etymon->id}}">{{$etymon->entry}}</a></li>
+    @endforeach
 @endsection
--- server/resources/views/lexicon/layout.blade.php
+++ server/resources/views/lexicon/layout.blade.php
@@ -69,6 +69,23 @@
             var id = select.options[select.selectedIndex].value;
             document.location.href = "/lexicon/"+window.lexicon_slug+"/language/"+id;
         }
+
+        function search_word_sidebar(value) {
+            var items = document.getElementById('sidebar-word-list').getElementsByTagName('li');
+            for (var i=0;i<items.length;i++) {
+                var item = items[i];
+                if (item.innerText.indexOf(value) === -1) {
+                    item.style.display = 'none';
+                } else {
+                    item.style.display = '';
+                }
+            }
+        }
+
+        function clear_word_search() {
+            document.getElementById('search_word_text').value='';
+            search_word_sidebar('');
+        }
     </script>
 </head>
 <body>
@@ -113,7 +130,6 @@
 
     TODO:
         all pages:
-            sidebar search bar (should filter currently-displayed list, i guess?)
             display data (reflexes on etyma pages, cognates on reflex pages, items in sidebar, etc) in some sensible order other than database-id order
             Semitilex 'Categories' on right-side bar - for IELEX these are Pokorny's list of 'Semantic Categories'.  What are they here?
         search page:
@@ -129,7 +145,59 @@
 </div>
 
 <div id="sidebar" class="sidebar col-lg-3 col-md-12 p-0" style="height:100vh;overflow-y:scroll">
-    @yield('search-sidebar')
+    <div id="sidebar-header" class="sidebar-header">
+        <p>
+            <a href="#" class="selector_type_link{{$selected_sidebar=='headword' ? ' selected' : ''}}"
+               onclick="choose_selector_type('headword');return false;" id="selector_type_link_headword">Headwords</a>
+            |
+            <a href="#" class="selector_type_link{{$selected_sidebar=='category' ? ' selected' : ''}}"
+               onclick="choose_selector_type('category');return false;" id="selector_type_link_category">Categories</a>
+        </p>
+    </div>
+    <div id="sidebar-content" class="sidebar-content">
+        <div class="selector_type" id="selector_type_headword"@if ($selected_sidebar!=='headword') style="display:none;"@endif>
+            <div class="sidebar-search-area">
+                <form id="search_form" onsubmit="search_word_sidebar(this.text.value);return false;" class="row row-cols-lg-auto align-items-center">
+                    <div class="col-9">
+                        <div class="input-group">
+                        <input type="text" id="search_word_text" name="text" class="form-control">
+                            <button class="btn btn-outline-secondary" style="background-color:white;" type="button" onclick="clear_word_search()">
+                                <svg width="24px" height="24px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5.72 5.72a.75.75 0 011.06 0L12 10.94l5.22-5.22a.75.75 0 111.06 1.06L13.06 12l5.22 5.22a.75.75 0 11-1.06 1.06L12 13.06l-5.22 5.22a.75.75 0 01-1.06-1.06L10.94 12 5.72 6.78a.75.75 0 010-1.06z"/></svg>
+                            </button>
+                        </div>
+                    </div>
+                    <div class="col-3">
+                        <button type="submit" class="btn btn-light">Search</button>
+                    </div>
+                </form>
+            </div>
+            <ul style="padding-left: 1rem;" id="sidebar-word-list">
+            @yield('search-item-list')
+            </ul>
+        </div>
+        <div class="selector_type" id="selector_type_category"@if ($selected_sidebar!=='category') style="display:none;"@endif>
+            <div class="accordion" id="accordionCategory">
+                @foreach ($lexicon->semantic_categories as $category)
+                    <div class="accordion-item">
+                        <h2 class="accordion-header" id="accordion_category_heading_{{$category->id}}">
+                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#accordion_category_collapse_{{$category->id}}" aria-expanded="true" aria-controls="accordion_category_collapse_{{$category->id}}">
+                                {{$category->text}}
+                            </button>
+                        </h2>
+                        <div id="accordion_category_collapse_{{$category->id}}" class="accordion-collapse collapse" aria-labelledby="accordion_category_heading_{{$category->id}}" data-bs-parent="#accordionCategory">
+                            <div class="accordion-body">
+                                <ul>
+                                    @foreach ($category->semantic_fields as $field)
+                                        <li data-sidebar-id="{{$field->id}}"><a href="/lexicon/{{$lexicon->slug}}/field/{{$field->id}}">{{$field->text}}</a></li>
+                                    @endforeach
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                @endforeach
+            </div>
+        </div>
+    </div>
 </div>
 </div>
 </div>
