49ed26dbd1b0c15f9b2009a1da236ee2df2c481e
Chris Pittman <chrispittman73@gmail.com>
2023-04-24 12:12:19 -0500

    fix: set 'user' table name explicitly
    fixes #138

--- server/config/permission.php
+++ server/config/permission.php
@@ -30,6 +30,8 @@ return [
 
     'table_names' => [
 
+        'users' => 'user',
+
         /*
          * When using the "HasRoles" trait from this package, we need to know which
          * table should be used to retrieve your roles. We have chosen a basic
